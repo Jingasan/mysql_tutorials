741ea6b5b75c370bf2f9d0c886cd9743a5e38f64
shota_niga <shota.niga@informatix.co.jp>
2023-02-17 22:00:57 +0900

    <add> initial commit

---
+++ 00_simple_mysql/db/sample_data/create_table_employee.sql
@@ -0,0 +1,21 @@
+CREATE TABLE
+	departments (
+		dept_id INT NOT NULL, -- 部門ID
+		dept_name VARCHAR(255) NOT NULL, -- 部署名
+		PRIMARY KEY (dept_id) -- 主キー
+	);
+
+CREATE TABLE
+	employees (
+		emp_id INT NOT NULL, -- 従業員番号
+		dept_id INT NOT NULL, -- 部署番号
+		emp_name VARCHAR(20) NOT NULL, -- 担当者名
+		birthday DATE NOT NULL, -- 生年月日
+		hiredate DATE NOT NULL, -- 入社年月日
+		sex INT NOT NULL, -- 性別
+		sal NUMERIC(9, 2), -- 給与額
+		comm NUMERIC(planet_osm_point7, 2), -- 歩合給
+		PRIMARY KEY (emp_id), -- 主キー
+		FOREIGN KEY (dept_id) -- 外部キー
+		REFERENCES departments (dept_id) -- 部署テーブル.部署番号
+	);
